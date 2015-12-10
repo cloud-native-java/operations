2d0cbadb95ef422edfdff19aa8df8461fe098ca2
Josh Long <josh@joshlong.com>
2015-12-10 03:09:04 -0600

    SUPER ROUGH

---
+++ src/main/java/demo/OperationalizedApplication.java
@@ -0,0 +1,13 @@
+package demo;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperationalizedApplication {
+
+    public static void main(String args[]) throws Exception {
+        SpringApplication.run(OperationalizedApplication.class, args);
+    }
+}
---
+++ src/test/java/demo/OperationalizedApplicationTest.java
@@ -0,0 +1,27 @@
+package demo;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = OperationalizedApplication.class)
+@WebIntegrationTest({"server.port=0"})
+public class OperationalizedApplicationTest {
+
+
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+}
