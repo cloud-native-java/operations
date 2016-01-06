0cd838298b84e8b3d8d6418973df321f72ce08b4
Josh Long <josh@joshlong.com>
2016-01-06 00:15:54 -0800

    moving tracing bits under the tracing directory so that i can stand up an adjacent rememdiation package and not have everything get too clunky

--- deploy.sh
+++ deploy.sh
@@ -9,50 +9,55 @@ function reset(){
     echo "deleted ${app_name}"
 }
 
-cd `dirname $0`
-root=`pwd`
+function tracing(){
 
-echo "`dirname $0` in operations/deploy.sh"
+    cd `dirname $0`
+    root=`pwd`/tracing
 
-source $BUILD_DIRECTORY/utils/cf-common.sh
+    echo "`dirname $0` in operations/deploy.sh"
 
-# create RabbitMQ
-rabbit=cnj-rabbitmq
-cf s | grep ${rabbit} && echo "found ${rabbit}" || cf cs cloudamqp lemur ${rabbit}
+    source $BUILD_DIRECTORY/utils/cf-common.sh
 
-# create MySQL DB
-mysql=cnj-mysql
-cf s | grep ${mysql} && echo "found ${mysql}" || cf cs cleardb spark ${mysql}
+    # create RabbitMQ
+    rabbit=cnj-rabbitmq
+    cf s | grep ${rabbit} && echo "found ${rabbit}" || cf cs cloudamqp lemur ${rabbit}
 
-# deploy zipkin-query-service
-zq=zipkin-query-service
-cd $root/$zq
-reset $zq
-cf d -f $zq
-cd $root/zipkin-query-service
-cf push
+    # create MySQL DB
+    mysql=cnj-mysql
+    cf s | grep ${mysql} && echo "found ${mysql}" || cf cs cleardb spark ${mysql}
 
-# deploy zipkin-web
-zw=zipkin-web
-reset $zw
-cf d -f $zw
-zqs_name=`app_domain $zq`
-cd $root/zipkin-web
-cf push --no-start
-jcjm=`$root/deploy-helper.py $zqs_name`
-cf set-env $zw JBP_CONFIG_JAVA_MAIN "${jcjm}"
-cf restart $zw
+    # deploy zipkin-query-service
+    zq=zipkin-query-service
+    cd $root/$zq
+    reset $zq
+    cf d -f $zq
+    cd $root/zipkin-query-service
+    cf push
 
-# deploy clients
-cd $root
-zc_a=zipkin-client-a
-zc_b=zipkin-client-b
-reset $zc_a
-reset $zc_b
+    # deploy zipkin-web
+    zw=zipkin-web
+    reset $zw
+    cf d -f $zw
+    zqs_name=`app_domain $zq`
+    cd $root/zipkin-web
+    cf push --no-start
+    jcjm=`$root/deploy-helper.py $zqs_name`
+    cf set-env $zw JBP_CONFIG_JAVA_MAIN "${jcjm}"
+    cf restart $zw
 
-cf s | grep $zc_b && cf ds -f $zc_b
-deploy_app $zc_b
-deploy_service $zc_b
+    # deploy clients
+    cd $root
+    zc_a=zipkin-client-a
+    zc_b=zipkin-client-b
+    reset $zc_a
+    reset $zc_b
 
-deploy_app $zc_a
+    cf s | grep $zc_b && cf ds -f $zc_b
+    deploy_app $zc_b
+    deploy_service $zc_b
+
+    deploy_app $zc_a
+}
+
+tracing
 
