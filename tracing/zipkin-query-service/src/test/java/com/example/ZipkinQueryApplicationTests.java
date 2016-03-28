3185e2f2249c0ed0bc41d350bd87c09716df0e1d
Josh Long <josh@joshlong.com>
2016-03-28 12:55:49 +0300

    trace

--- tracing/zipkin-query-service/src/test/java/com/example/ZipkinQueryApplicationTests.java
+++ tracing/zipkin-query-service/src/test/java/com/example/ZipkinQueryApplicationTests.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = ZipkinQueryApplication.class)
+@SpringApplicationConfiguration(classes = ZipkinApplication.class)
 public class ZipkinQueryApplicationTests {
 
 	@Test
