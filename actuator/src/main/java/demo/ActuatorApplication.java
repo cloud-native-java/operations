787f21b1851c5d4b59bf86e851f9a45203c6053b
Josh Long <josh@joshlong.com>
2016-03-30 00:56:32 -0600

    actuator application

--- actuator/src/main/java/demo/ActuatorApplication.java
+++ actuator/src/main/java/demo/ActuatorApplication.java
@@ -5,14 +5,10 @@ import com.codahale.metrics.graphite.Graphite;
 import com.codahale.metrics.graphite.GraphiteReporter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.autoconfigure.ExportMetricWriter;
-import org.springframework.boot.actuate.metrics.statsd.StatsdMetricWriter;
-import org.springframework.boot.actuate.metrics.writer.MetricWriter;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import javax.annotation.PostConstruct;
-import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
@@ -20,31 +16,16 @@ public class ActuatorApplication {
 
 	@PostConstruct
 	public void begin() throws Exception {
-		// prevent DNS caching because
-		// HostedGraphite nodes may move
+		// prevent DNS caching because HostedGraphite nodes may move
 		java.security.Security.setProperty("networkaddress.cache.ttl", "60"); // <1>
 	}
 
-//	@Bean
-//	@ExportMetricWriter
-//	MetricWriter metricWriter(
-//			@Value("${hostedGraphite.apiKey}") String key,
-//			@Value("${hostedGraphite.url}") String host,
-//			@Value("${hostedGraphite.port}") int port) {
-//		return new StatsdMetricWriter(key, host, port);
-//	}
-
-
 	@Bean
-	GraphiteReporter graphiteWriter(MetricRegistry registry) {
-
-		// prevent DNS caching because
-		// HostedGraphite nodes may move
-		java.security.Security.setProperty("networkaddress.cache.ttl", "60"); // <1>
-
-		int port = 2003;
-		String apiKey = "b1f80e86-35ad-458b-a391-1ba8d6533e99";
-		String host = "672c4ee8.carbon.hostedgraphite.com";
+	GraphiteReporter graphiteWriter(
+			@Value("${hostedGraphite.apiKey}") String apiKey, // NB: we're using the API KEY as a prefix
+			@Value("${hostedGraphite.url}") String host,
+			@Value("${hostedGraphite.port}") int port,
+			MetricRegistry registry) {
 
 		GraphiteReporter reporter = GraphiteReporter.forRegistry(registry)
 				.prefixedWith(apiKey) // <2>
