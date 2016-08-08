e8a9bde14307f8e25e61c0359193f0e78014edb3
Josh Long <josh@joshlong.com>
2016-08-08 10:08:30 +0100

    adding routing

--- spring-boot-admin/spring-boot-admin-client/src/main/java/com/example/SpringBootAdminClientApplication.java
+++ spring-boot-admin/spring-boot-admin-client/src/main/java/com/example/SpringBootAdminClientApplication.java
@@ -2,9 +2,11 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @RestController
 public class SpringBootAdminClientApplication {
--- spring-boot-admin/spring-boot-admin-server/src/main/java/com/example/SpringBootAdminApplication.java
+++ spring-boot-admin/spring-boot-admin-server/src/main/java/com/example/SpringBootAdminApplication.java
@@ -3,7 +3,9 @@ package com.example;
 import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @EnableAdminServer
 @SpringBootApplication
 public class SpringBootAdminApplication {
