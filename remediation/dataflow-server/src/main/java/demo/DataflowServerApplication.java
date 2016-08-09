4a794adfc45b031d4a721600a7b82e702675bb51
Josh Long <josh@joshlong.com>
2016-08-09 09:47:23 +0100

    parent

--- remediation/dataflow-server/src/main/java/demo/DataflowServerApplication.java
+++ remediation/dataflow-server/src/main/java/demo/DataflowServerApplication.java
@@ -2,6 +2,7 @@ package demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.dataflow.server.EnableDataFlowServer;
 
 @SpringBootApplication
