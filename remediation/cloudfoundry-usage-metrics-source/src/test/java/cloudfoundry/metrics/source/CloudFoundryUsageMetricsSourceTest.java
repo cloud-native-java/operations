c5d1532138720f3d7930c3db9129bb15c864ad6a
Josh Long <josh@joshlong.com>
2017-05-14 12:35:37 +0200

    os

--- remediation/cloudfoundry-usage-metrics-source/src/test/java/cloudfoundry/metrics/source/CloudFoundryUsageMetricsSourceTest.java
+++ remediation/cloudfoundry-usage-metrics-source/src/test/java/cloudfoundry/metrics/source/CloudFoundryUsageMetricsSourceTest.java
@@ -5,6 +5,9 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.cloudfoundry.operations.CloudFoundryOperations;
 import org.cloudfoundry.operations.applications.GetApplicationRequest;
+import org.cloudfoundry.operations.applications.StartApplicationRequest;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,12 +17,21 @@ import org.springframework.cloud.stream.test.binder.MessageCollector;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
 import org.springframework.messaging.Message;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.StreamUtils;
 import org.springframework.util.StringUtils;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.file.Files;
 import java.util.Map;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
@@ -41,17 +53,75 @@ public class CloudFoundryUsageMetricsSourceTest {
     private MessageCollector messageCollector;
 
     @Autowired
-    private CloudFoundryOperations cloudFoundryClient;
+    private CloudFoundryOperations ops;
 
     @Autowired
     private RestTemplate restTemplate;
 
+    private void stage() throws Throwable {
+        Assert.assertTrue(tempRootForStaging.exists() || tempRootForStaging.mkdirs());
+        Resource manifest = new ClassPathResource("/manifest.yml");
+        Resource jar = new FileSystemResource(new File(
+                "./src/integration-test/resources/hi.jar"));
+        try (InputStream mi = manifest.getInputStream();
+             InputStream ji = jar.getInputStream();
+             OutputStream mo = new FileOutputStream(manifestFile);
+             OutputStream jo = new FileOutputStream(jarFile)) {
+            StreamUtils.copy(mi, mo);
+            StreamUtils.copy(ji, jo);
+        }
+        log.info("staging application at " + tempRootForStaging.getAbsolutePath());
+
+        Assert.assertTrue(
+                "the hi.jar should exist at " + this.jarFile.getAbsolutePath(),
+                this.jarFile.exists());
+
+        Assert.assertTrue(
+                "the manifest.yml should exist at " + this.manifestFile.getAbsolutePath(),
+                this.manifestFile.exists());
+    }
+
     @Autowired
     private CloudFoundryUsageMetricsSourceProperties properties;
 
-    String urlFor(String appName) {
+    private String applicationName;
+
+    private File tempRootForStaging, manifestFile, jarFile;
+
+    private void deploySampleApp() throws Throwable {
+        this.stage();
+
+        this.applicationName = this.cloudFoundryService
+                .applicationManifestFrom(this.manifestFile).entrySet().stream()
+                .map(e -> e.getValue().getName()).findAny().orElse(null);
+        if (!this.cloudFoundryService.applicationExists(this.applicationName)) {
+            this.cloudFoundryService.pushApplicationUsingManifest(this.manifestFile);
+        }
+
+        if (cloudFoundryService.applicationExists(this.applicationName)) {
+            this.ops
+                    .applications()
+                    .start(StartApplicationRequest.builder().name(this.applicationName).build())
+                    .block();
+        }
+
+    }
+
+    @Before
+    public void before() throws Throwable {
+        this.tempRootForStaging = Files.createTempDirectory("staging").toFile();
+
+        this.manifestFile = new File(tempRootForStaging, "manifest.yml");
+        this.jarFile = new File(tempRootForStaging, "hi.jar");
+
+        this.deploySampleApp();
+
+
+    }
+
+    private String urlFor(String appName) {
         return
-                "http://" + (this.cloudFoundryClient
+                "http://" + (this.ops
                         .applications()
                         .get(GetApplicationRequest.builder().name(appName).build())
                         .map(applicationDetail -> applicationDetail.getUrls().iterator().next())
